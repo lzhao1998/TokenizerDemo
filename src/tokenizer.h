247109289389511cd03764a6e8f794c25c0d6416
Lai Zhe Hao <lzhao1998@gmail.com>
2017-06-24 14:00:17 +0800

    Created Tokenizer project

---
+++ src/tokenizer.h
@@ -0,0 +1,5 @@
+#ifndef _TOKENIZER_H
+#define _TOKENIZER_H
+
+
+#endif // _TOKENIZER_H
---
+++ test/test_tokenizer.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "tokenizer.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_tokenizer_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement tokenizer");
+}
